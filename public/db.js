09ba90ac56ec3eb4c0dc426b2629cc59124c6cd4
Melissa Brewer <melramirez727@gmail.com>
2021-09-11 21:42:37 -0500

    getAll.onsuccess

--- public/db.js
+++ public/db.js
@@ -21,3 +21,36 @@ request.onupgradeneeded = function (e) {
   request.onerror = function (e) {
     console.log(`Woops! ${e.target.errorCode}`);
   };
+
+  function checkDatabase() {
+    console.log('check db invoked');
+  
+      let transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
+    const store = transaction.objectStore('BudgetStore');
+  
+       const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((res) => {
+                 if (res.length !== 0) {
+                 transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
+                const currentStore = transaction.objectStore('BudgetStore');
+                currentStore.clear();
+              console.log('Clearing store 🧹');
+            }
+          });
+      }
+    };
+  } 
